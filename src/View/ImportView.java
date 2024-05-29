08553ddd005c6c0755f4bd986930a1f2fb8d6b18
bus94 <lbs927@naver.com>
2024-05-29 11:52:49 +0900

    상세 페이지 메서드 만들기

--- src/View/ImportView.java
+++ src/View/ImportView.java
@@ -1,6 +1,7 @@
 package View;
 
 import java.awt.Color;
+import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.GridLayout;
@@ -26,7 +27,7 @@ import javax.swing.table.TableColumnModel;
 import DatePickerEx.JDatePickerEx;
 
 public class ImportView extends JPanel {
-	
+
 	JPanel panMain;
 	private String day;
 	private int price;
@@ -35,12 +36,14 @@ public class ImportView extends JPanel {
 
 	private JScrollPane sp = new JScrollPane();
 	private JTabbedPane tabPanel = new JTabbedPane();
-	
+	private JPanel mainPanel, btnPanel, checkPanel, totalPanel, dayPanel, monthPanel, btnsPanel, updatePanel,
+			datePanelL, datePan, amountPanelL, amountPanelR, typePanelL, typePanelR, memoPanelL, memoPanelR;
+
 	public ImportView() {
 	}
 
 	public ImportView(JPanel panel) {
-		panMain = panel; 
+		panMain = panel;
 		print();
 
 	}
@@ -55,22 +58,22 @@ public class ImportView extends JPanel {
 	public void print() {
 		Rectangle rect = panMain.getBounds();
 		setPreferredSize(rect.getSize());
-		setBounds(50, 50, 1200, 800);
+		panMain.setBounds(7, 0, 1170, 765);
 		setLayout(null);
 
 		// main 패널 (tab 생성 포함)
-		JPanel mainPanel = new JPanel();
+		mainPanel = new JPanel();
 		mainPanel.setBorder(new LineBorder(Color.green, 8));
 		mainPanel.setBackground(Color.white);
 		mainPanel.setBounds(0, 0, 1170, 750);
 		mainPanel.setLayout(null);
 
 		// 메뉴 패널(메인으로 돌아가기 버튼)
-		JPanel btnPanel = new JPanel();
+		btnPanel = new JPanel();
 		btnPanel.setBackground(Color.white);
 		JButton menuBtn = new JButton("메인으로");
 		menuBtn.addActionListener(new ActionListener() {
-			
+
 			// 버튼 클릭 시 메인으로 돌아가는 이벤트
 			@Override
 			public void actionPerformed(ActionEvent e) {
@@ -80,56 +83,27 @@ public class ImportView extends JPanel {
 				panMain.repaint();
 			}
 		});
-		
+
 		btnPanel.add(menuBtn);
 		btnPanel.setBounds(1050, 20, 100, 50);
 		mainPanel.add(btnPanel);
 
 		// 조회 패널(전체, 일별, 월별 tab패널)
-		JPanel checkPanel = new JPanel();
+		checkPanel = new JPanel();
 		checkPanel.setBounds(0, 0, 800, 600);
 		checkPanel.setBackground(Color.white);
 
-		// 전체 조회 패널
-		JPanel totalPanel = new JPanel();
-		totalPanel.setBounds(0, 0, 800, 600);
-		totalPanel.setBackground(Color.white);
-		String[] column = { "날짜", "금        액", "구분", "비        고" };
-		String[][] rowData = { { "24/05/27", "3000000", "급여", "보너스 제외" }, { "24/05/27", "1000000", "급여", "보너스" } };
-		sp = totalCheck(rowData, column);
-		totalPanel.add(sp);
-
-		// 일별 조회
-		JPanel dayPanel = new JPanel();
-		dayPanel.setBounds(100, 50, 1000, 600);
-		dayPanel.setBackground(Color.white);
-		// data DB 연결 필요
-		String[] column1 = { "날짜", "금        액", "구분", "비        고" };
-		String[][] rowData1 = { { "24/05/27", "4000000", "급여", "보너스 제외 및 알바비 포함" },
-				{ "24/05/27", "2000", "이자", "은행 이자" } };
-		sp = totalCheck(rowData1, column1);
-		dayPanel.add(sp);
-
-		// 월별 조회
-		JPanel monthPanel = new JPanel();
-		monthPanel.setBounds(100, 50, 1000, 600);
-		monthPanel.setBackground(Color.white);
-		String[] column2 = { "날짜", "금        액", "구분", "비        고" };
-		String[][] rowData2 = { { "24/05/27", "5000000", "급여", "보너스 포함" }, { "24/05/27", "1000000", "기타", "길에서 주움" } };
-		sp = totalCheck(rowData2, column2);
-		monthPanel.add(sp);
-
 		// tab
-		tabPanel.add("전체", totalPanel);
-		tabPanel.add("일별", dayPanel);
-		tabPanel.add("월별", monthPanel);
+		tabPanel.add("전체", totalPanel());
+		tabPanel.add("일별", dayPanel());
+		tabPanel.add("월별", monthPanel());
 
 		checkPanel.add(tabPanel);
 		checkPanel.setBounds(40, 50, 750, 600);
 		mainPanel.add(checkPanel);
 
 		// 버튼 패널(추가, 수정, 삭제)
-		JPanel btnsPanel = new JPanel();
+		btnsPanel = new JPanel();
 		btnsPanel.setBounds(870, 600, 250, 100);
 		btnsPanel.setBackground(Color.white);
 
@@ -167,12 +141,12 @@ public class ImportView extends JPanel {
 		});
 
 		// 수정 패널(추가, 수정, 삭제)
-		JPanel updatePanel = new JPanel(new GridLayout(4, 2));
+		updatePanel = new JPanel(new GridLayout(4, 2));
 		updatePanel.setBounds(650, 250, 500, 300);
 		updatePanel.setBackground(Color.white);
 
 		// 날짜 패널(왼쪽)
-		JPanel datePanelL = new JPanel(new FlowLayout(FlowLayout.RIGHT)); // 오른쪽부터 정렬
+		datePanelL = new JPanel(new FlowLayout(FlowLayout.RIGHT)); // 오른쪽부터 정렬
 		JLabel dateLabel = new JLabel("날    짜 : ", 10);
 		datePanelL.add(dateLabel);
 		datePanelL.setBackground(Color.white);
@@ -180,34 +154,34 @@ public class ImportView extends JPanel {
 
 		// 날짜 패널(오른쪽)
 		JDatePickerEx date = new JDatePickerEx();
-		JPanel datePan = date.datePanel();
+		datePan = date.datePanel();
 		datePan.setLayout(new FlowLayout(FlowLayout.LEFT));
 		datePan.setBackground(Color.white);
 		updatePanel.add(datePan);
 
 		// 금액 패널(왼쪽)
-		JPanel amountPanelL = new JPanel(new FlowLayout(FlowLayout.RIGHT));
+		amountPanelL = new JPanel(new FlowLayout(FlowLayout.RIGHT));
 		JLabel amountLabel = new JLabel("금    액 : ", 10);
 		amountPanelL.add(amountLabel);
 		amountPanelL.setBackground(Color.white);
 		updatePanel.add(amountPanelL);
 
 		// 금액 패널(오른쪽)
-		JPanel amountPanelR = new JPanel(new FlowLayout(FlowLayout.LEFT)); // 왼쪽부터 정렬
+		amountPanelR = new JPanel(new FlowLayout(FlowLayout.LEFT)); // 왼쪽부터 정렬
 		JTextField amountField = new JTextField(20);
 		amountPanelR.add(amountField);
 		amountPanelR.setBackground(Color.white);
 		updatePanel.add(amountPanelR);
 
 		// 유형 패널(왼쪽)
-		JPanel typePanelL = new JPanel(new FlowLayout(FlowLayout.RIGHT));
+		typePanelL = new JPanel(new FlowLayout(FlowLayout.RIGHT));
 		JLabel typeLabel = new JLabel("유    형 : ", 10);
 		typePanelL.add(typeLabel);
 		typePanelL.setBackground(Color.white);
 		updatePanel.add(typePanelL);
 
 		// 유형 패널(오른쪽)
-		JPanel typePanelR = new JPanel(new FlowLayout(FlowLayout.LEFT)); // 왼쪽부터 정렬
+		typePanelR = new JPanel(new FlowLayout(FlowLayout.LEFT)); // 왼쪽부터 정렬
 		String[] exportType = { "급여", "이자", "기타" };
 		JComboBox typeBox = new JComboBox(exportType);
 		typePanelR.add(typeBox);
@@ -215,14 +189,14 @@ public class ImportView extends JPanel {
 		updatePanel.add(typePanelR);
 
 		// 비고 패널(왼쪽)
-		JPanel memoPanelL = new JPanel(new FlowLayout(FlowLayout.RIGHT));
+		memoPanelL = new JPanel(new FlowLayout(FlowLayout.RIGHT));
 		JLabel memoLabel = new JLabel("비    고 : ", 10);
 		memoPanelL.add(memoLabel);
 		memoPanelL.setBackground(Color.white);
 		updatePanel.add(memoPanelL);
 
 		// 비고 패널(오른쪽)
-		JPanel memoPanelR = new JPanel(new FlowLayout(FlowLayout.LEFT)); // 왼쪽부터 정렬
+		memoPanelR = new JPanel(new FlowLayout(FlowLayout.LEFT)); // 왼쪽부터 정렬
 		JTextField memoField = new JTextField(20);
 		memoPanelR.add(memoField);
 		memoPanelR.setBackground(Color.white);
@@ -308,6 +282,43 @@ public class ImportView extends JPanel {
 		totalTable.getColumn("비        고").setCellRenderer(dtcrCenter);
 
 		return scrollpane;
+	}
+
+	// 전체 조회 패널
+	public JPanel totalPanel() {
+		totalPanel = new JPanel();
+		totalPanel.setBounds(0, 0, 800, 600);
+		totalPanel.setBackground(Color.white);
+		String[] column = { "날짜", "금        액", "구분", "비        고" };
+		String[][] rowData = { { "24/05/27", "3000000", "급여", "보너스 제외" }, { "24/05/27", "1000000", "급여", "보너스" } };
+		sp = totalCheck(rowData, column);
+		totalPanel.add(sp);
+		return totalPanel;
+	}
+
+	// 일별 조회
+	public JPanel dayPanel() {
+		dayPanel = new JPanel();
+		dayPanel.setBounds(100, 50, 1000, 600);
+		dayPanel.setBackground(Color.white);
+		String[] column1 = { "날짜", "금        액", "구분", "비        고" };
+		String[][] rowData1 = { { "24/05/27", "4000000", "급여", "보너스 제외 및 알바비 포함" },
+				{ "24/05/27", "2000", "이자", "은행 이자" } };
+		sp = totalCheck(rowData1, column1);
+		dayPanel.add(sp);
+		return dayPanel;
+	}
+
+	// 월별 조회
+	public JPanel monthPanel() {
+		monthPanel = new JPanel();
+		monthPanel.setBounds(100, 50, 1000, 600);
+		monthPanel.setBackground(Color.white);
+		String[] column2 = { "날짜", "금        액", "구분", "비        고" };
+		String[][] rowData2 = { { "24/05/27", "5000000", "급여", "보너스 포함" }, { "24/05/27", "1000000", "기타", "길에서 주움" } };
+		sp = totalCheck(rowData2, column2);
+		monthPanel.add(sp);
+		return monthPanel;
 	}
 
 }
