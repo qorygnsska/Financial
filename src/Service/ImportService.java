116f577ebec4f1bb94d93d46f86275d2565fc592
jhhim <jhhim99@gmail.com>
2024-05-29 12:54:34 +0900

    메인 유저패널 추가

--- src/Service/ImportService.java
+++ src/Service/ImportService.java
@@ -7,7 +7,7 @@ public class ImportService {
 	private ImportDAO dao = new ImportDAO();
 	
 	// 전제 조회 후 테이블 출력
-	public String[][] select() {
+	public Object[][] select() {
 			
 		return dao.select();		
 	}
