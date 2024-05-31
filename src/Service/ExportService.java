9e325253cec39bb35d38aed354899157e1863fad
bus94 <lbs927@naver.com>
2024-05-31 16:25:25 +0900

    bs

--- src/Service/ExportService.java
+++ src/Service/ExportService.java
@@ -16,4 +16,15 @@ public class ExportService {
 		return dao.add(exportModel);
 	}
 
+	// 선택한 날짜의 값이 있는지 확인
+	public boolean selectCheck(String date) {
+		System.out.println("(ExportService)선택한 날짜 조회 중");
+		return dao.selectCheck(date);
+	}
+
+	// 선택한 날짜의 값을 불러오는 메서드
+	public String[][] selectDay(String date) {
+		return dao.selectDay(date);
+	}
+
 }
