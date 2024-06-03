403e5db474d36f2377c8eccc65e10815f46c0004
bus94 <lbs927@naver.com>
2024-06-03 11:43:55 +0900

    2

--- src/View/ImportView.java
+++ src/View/ImportView.java
@@ -134,10 +134,7 @@ public class ImportView extends JPanel {
 			selectrownum = selectRow + 1;
 			// 선택한 행이 있는지 확인
 			if (selectRow != -1) {
-<<<<<<< HEAD
-=======
 			
->>>>>>> 602910db9a793056470123529c19d7ee8b531ec1
 				String datetext = (String)dayTable.getValueAt(selectRow, 0);
 				System.out.println((String)dayTable.getValueAt(selectRow, 0));
 				
