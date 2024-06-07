d99882a9a5139a39d2aa4d4bd40c1f17062707f3
hong6425 <61792051+hong6425@users.noreply.github.com>
2024-06-07 19:21:28 +0900

    .

--- src/DAO/DBUtil.java
+++ src/DAO/DBUtil.java
@@ -11,7 +11,7 @@ public class DBUtil {
 		try {
 			Class.forName("oracle.jdbc.driver.OracleDriver");
 
-			String url = "jdbc:oracle:thin:@localhost:1521:xe";
+			String url = "jdbc:oracle:thin:@localhost:1521:orcl";
 			String user = "a1234";
 			String password = "1234";
 
